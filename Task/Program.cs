ee3b7a0703b3e12e44f50e2d17b7721ee1a30821
Ridzhin <126336074+Ridzhin@users.noreply.github.com>
2023-04-27 18:31:49 +0300

    Add files via upload

---
+++ Task/Program.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using Task;
+namespace InterfaceTask 
+{ 
+class Program
+{ 
+    static ILogger Logger { get; set; }  //Логгер из скринкаста
+
+    static void Main()
+    { 
+
+        Logger = new Logger();
+       
+        var worker1 = new Worker1(Logger);
+        worker1.Work();
+
+        Console.ReadKey();
+        
+    } 
+}
+
+public interface ILogger
+{
+    void Event(string messege);
+    void Error(string messege);
+}
+
+public class Logger : ILogger 
+{
+    public void Error(string messege)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(messege);
+    }
+
+    public void Event(string messege)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine(messege);
+
+    }
+    public interface IWorker
+    {
+        void Work();
+    }
+}
+}
---
+++ Task/Worker1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using InterfaceTask;
+using static InterfaceTask.Logger;
+
+namespace Task
+{
+    public class Worker1 : IWorker
+    {
+        ILogger Logger { get; }
+        public Worker1(ILogger logger)
+        { 
+            Logger = logger;
+        }
+
+        public void Work()   //калькулятор с конструкцией try/catch
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;    //cообщение синее
+                    Console.WriteLine("Введите х");
+                    double x = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Введите y");
+                    double y = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("{0} + {1} = {2}", x, y, x + y);
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red; // если ошибка красное
+                    Console.WriteLine("Ошибка ввода");
+                   
+                }
+            }
+        }
+
+    }
+}
